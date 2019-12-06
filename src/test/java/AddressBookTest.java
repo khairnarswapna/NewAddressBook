110ecf50602c56c16a3f241636bcb934aaa344a6
khairnarswapna <khairnarswapna99@gmail.com>
2019-12-06 11:27:39 +0530

    add the person info into Json File

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -13,7 +13,7 @@ public class AddressBookTest {
     @Test
     public void givenPersonInWrittenToJson_Should_ReturnTrue() throws FileNotFoundException {
         Address address = new Address("Dhule  ", "100345", "Mh");
-        Person person = new Person("puja", "Patil", address, "9405205050");
+        Person person = new Person("Puja", "Patil", address, "9405205050");
         //Manager manager = new AddressBookManager();
         boolean result =iAddressBook.addPerson(person, "/home/admin142/eclipse-workspace/AddressBook2/src/main/resources/Person.json");
         Assert.assertEquals(true, result);
